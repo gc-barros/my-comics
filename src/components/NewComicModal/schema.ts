30b7782245d9836caa39e40f3587ef698d7d706b
Gabriel Barros <super.gcb18@gmail.com>
2025-06-03 20:44:49 -0300

    refactor: simplificando o schema

--- src/components/NewComicModal/schema.ts
+++ src/components/NewComicModal/schema.ts
@@ -1,16 +1,20 @@
 import { z } from 'zod';
 
+const statusEnum = z.enum(['not_started', 'in_progress', 'finished'], {
+  required_error: 'Please select a status',
+});
+
+export type Status = z.infer<typeof statusEnum>;
+
 export const comicFormSchema = z
   .object({
-    status: z.enum(['not_started', 'in_progress', 'finished'], {
-      required_error: 'Please select a status',
-    }),
+    status: statusEnum.nullable().pipe(statusEnum),
     plannedStartDate: z
       .date({
         required_error: 'Please select a start date',
       })
       .refine(
-        date => date >= new Date(new Date().setHours(0, 0, 0, 0)),
+        (date: Date) => date >= new Date(new Date().setHours(0, 0, 0, 0)),
         'Start date must be today or later'
       )
       .nullable(),
@@ -30,41 +34,17 @@ export const comicFormSchema = z
       .max(5, 'Rating must be between 1 and 5')
       .nullable(),
   })
-  .refine(
-    schema => {
-      if (schema.status === 'not_started' && !schema.plannedStartDate) {
-        return false;
-      }
-      return true;
-    },
-    {
-      message: 'Start date is required when status is "Not Started"',
-      path: ['plannedStartDate'],
-    }
-  )
-  .refine(
-    schema => {
-      if (schema.status === 'in_progress' && !schema.currentPage) {
-        return false;
-      }
-      return true;
-    },
-    {
-      message: 'Current page is required when status is "In Progress"',
-      path: ['currentPage'],
-    }
-  )
-  .refine(
-    schema => {
-      if (schema.status === 'finished' && !schema.rating) {
-        return false;
-      }
-      return true;
-    },
-    {
-      message: 'Rating is required when status is "Finished"',
-      path: ['rating'],
-    }
-  );
+  .refine(schema => !(schema.status === 'not_started' && !schema.plannedStartDate), {
+    message: 'Start date is required when status is "Not Started"',
+    path: ['plannedStartDate'],
+  })
+  .refine(schema => !(schema.status === 'in_progress' && !schema.currentPage), {
+    message: 'Current page is required when status is "In Progress"',
+    path: ['currentPage'],
+  })
+  .refine(schema => !(schema.status === 'finished' && !schema.rating), {
+    message: 'Rating is required when status is "Finished"',
+    path: ['rating'],
+  });
 
 export type ComicFormData = z.infer<typeof comicFormSchema>;
